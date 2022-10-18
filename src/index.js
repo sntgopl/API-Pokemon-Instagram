968b3f6fec9ed280dc5cdd79459eb964623ddcf5
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-18 11:09:11 -0500

    Set layout - html version

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+
+const closeBtn = document.querySelector('#close-btn');
+
+closeBtn.addEventListener('click', () => {
+  closeBtn.parentNode.style.display = 'none';
+});
