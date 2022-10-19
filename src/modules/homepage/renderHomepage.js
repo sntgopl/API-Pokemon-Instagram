648732b4a3e4d4e5e75a5cd2b1c25f53776cf936
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-19 16:56:25 -0300

    Add render homepage function

--- src/modules/homepage/renderHomepage.js
+++ src/modules/homepage/renderHomepage.js
@@ -1,3 +1,15 @@
-const pokeImg = document.querySelectorAll('.pokemon-img');
+const pokeCard = document.querySelectorAll('.card');
+const pokeImg = document.querySelectorAll('.pokemon-img').src;
 const pokeName = document.querySelectorAll('.pokemon-name');
 const like = document.querySelectorAll('.heart');
+
+const renderHomepage = (data) => {
+  const renderCard = `
+<img class="pokemon-img" src="${data.spriites.front_default}" alt="Pokemon Image">
+<div class="card-data">
+  <h2 class="pokemon-name">${data.name}</h2>
+  <div class="like-section"><i class="fa-regular fa-heart heart"></i><p>5 likes</p></div>
+</div>
+  `;
+  pokeCard.innerHTML = renderCard;
+}
