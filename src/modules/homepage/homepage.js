9313b178145724badb7f1cf9bb7a0cd2c8e88c0d
sntgopl <santi.ponsle@gmail.com>
2022-10-20 19:12:17 -0300

    Add like function

--- src/modules/homepage/homepage.js
+++ src/modules/homepage/homepage.js
@@ -1,44 +1,47 @@
 import getPokemon from '../apis/getPokemon.js';
 import getComments from '../popup/getComments.js';
 import { popup, renderPopup, homeSection } from '../popup/renderPopup.js';
+import { addLike } from './likes.js';
 
 const pokemonList = [];
 
 const mmm = (data) => {
-		pokemonList.push(data);
+  pokemonList.push(data);
 };
 
 const write = (list) => {
-	homeSection.innerHTML = list.map((data) => `
+  homeSection.innerHTML = list.map((data) => `
 		<div class="card">
 		<img class="pokemon-img" src="${data.sprites.front_default}" alt="Pokemon img">
 		<div class="card-data">
-			<h2 class="pokemon-name">${data.name.toUpperCase().slice(0,1)+data.name.slice(1, data.name.length)}</h2>
-			<div class="like-section"><i class="fa-regular fa-heart heart"></i><p>5 likes</p></div>
+			<h2 class="pokemon-name">${data.name.toUpperCase().slice(0, 1) + data.name.slice(1, data.name.length)}</h2>
+			<div class="like-section"><i class="fa-regular fa-heart heart"></i><p class="likes"></p></div>
 		</div>
 		<button class="comment-button">Comments</button>
 		<p class="number">#${data.id.toString().padStart(3, 0)}</p>
 		</div>
 		`)
-		.join(' ');
-		const commentBtn = document.querySelectorAll('.comment-button');
-		commentBtn.forEach((button, index) => {
-			button.addEventListener('click', () => {
-				getPokemon(index + 1, renderPopup);
-				getComments(index + 1);
-				popup.style.display = 'flex';
-				homeSection.style.display = 'none';
-				console.log(index)
-			})
-		})
-}
+    .join(' ');
+  const commentBtn = document.querySelectorAll('.comment-button');
+  commentBtn.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      getPokemon(index + 1, renderPopup);
+      getComments(index + 1);
+      popup.style.display = 'flex';
+      homeSection.style.display = 'none';
+    });
+  });
+	const likeBtn = document.querySelectorAll('.likes');
+	likeBtn.forEach((button, index) => {
+		button.addEventListener('click', addLike(index))
+	})
+};
 
 const fetchList = async (n) => {
-    for (let i = 1; i < n; i += 1) {
-			await getPokemon(i, mmm);
-    }
-		write(pokemonList);
-		console.log(pokemonList);
-}
+  for (let i = 1; i < n; i += 1) {
+    await getPokemon(i, mmm);
+  }
+  write(pokemonList);
+};
 
 export default fetchList;
--- src/modules/homepage/likes.js
+++ src/modules/homepage/likes.js
@@ -1,36 +1,48 @@
 class Like {
-    constructor(likes, id) {
-        this.likes = likes;
-        this.item_id = id;
-    }
+  constructor(likes, id) {
+    this.likes = likes;
+    this.item_id = id;
+  }
 }
 
-const addLike = async () => {
-    const likes = new Like(1, '1');
-    const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-    const appId = 'IJd6Da8dwtRjMMAE92Va';
-    const url = `${api}${appId}/comments`;
-  
-    const response = fetch(`${url}`, {
-      method: 'POST',
-      body: JSON.stringify(likes),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    const fetched = await (await response).text();
-    console.log(fetched); // eslint-disable-line
+const addLike = async (id) => {
+  const likes = new Like(1, id);
+  const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const appId = 'IJd6Da8dwtRjMMAE92Va';
+  const url = `${api}${appId}/likes`;
+
+  const response = fetch(`${url}`, {
+    method: 'POST',
+    body: JSON.stringify(likes),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const fetched = await (await response).text();
+  //  console.log(fetched); // eslint-disable-line
 };
 
 const getLikes = async (id) => {
-    const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-    const appId = 'IJd6Da8dwtRjMMAE92Va';
-    const url = `${api}${appId}/comments?item_id=${id}`;
-  
-    const response = fetch(`${url}`);
-    const data = await (await response).json();
-    renderLikes(data);
-};
-  
+  const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const appId = 'IJd6Da8dwtRjMMAE92Va';
+  const url = `${api}${appId}/likes?item_id=${id}`;
 
-export { addLike, getLikes };
+  const response = fetch(`${url}`);
+  const data = await (await response).json()
+    .then((data) => {
+        const e = document.createElement('p');
+        e.classList.add('likes');
+        e.innerHTML = `${data[id].likes}`;
+        const likeDiv = document.querySelectorAll('.like-section');
+        likeDiv[id].appendChild(e);
+        console.log(data);        
+    });
+};
+
+const getAllLikes = async (n) => {
+    for (let i = 0; i < n; i += 1) {
+        await getLikes(i);
+    }
+}
+
+export { addLike, getAllLikes };
