6190c148ecaa0b15188c3089beaad2cff1906be4
sntgopl <santi.ponsle@gmail.com>
2022-10-20 14:55:26 -0300

    Call function to index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import getPokemon from './modules/apis/getPokemon.js';
 // import newAppId from './modules/apis/newAppId.js';
 // import postComment from './modules/popup/postComment.js';
 import getComments from './modules/popup/getComments.js';
-import renderPopup from './modules/popup/renderPopup.js';
+import { renderPopup } from './modules/popup/renderPopup.js';
+import homeSection from './modules/homepage/homepage.js';
 
 const openBtn = document.querySelector('#tmp-popup-open');
 const popup = document.querySelector('#popup');
+
+
 
 
 openBtn.addEventListener('click', () => {
